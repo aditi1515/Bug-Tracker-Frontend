4942c3049e924f447b540c4bfc201894ef4dfdc1
Aditi Gemini <aditi152003@gmail.com>
2024-03-28 00:42:56 +0530

    superAdmin analytics dashboard

--- utils/FormDataGenerator.js
+++ utils/FormDataGenerator.js
@@ -1,58 +1,48 @@
 function convertDataToFormData(
- data,
- formData = new FormData(),
- parentKey = ""
+  data,
+  formData = new FormData(),
+  parentKey = ""
 ) {
- if (parentKey === "") console.log("Data: ", data);
- for (let key in data) {
-  if (key === "logo")
-   console.log(
-    "key : ",
-    parentKey,
-    key,
-    "Value: ",
-    typeof data[key],
-    data[key]
-   );
-  if (data.hasOwnProperty(key)) {
-   const value = data[key];
-   const newKey = parentKey ? `${parentKey}[${key}]` : key;
+  for (let key in data) {
+    if (data.hasOwnProperty(key)) {
+      const value = data[key];
+      const newKey = parentKey ? `${parentKey}[${key}]` : key;
 
-   if (value instanceof FileList) {
-    for (let i = 0; i < value.length; i++) {
-     console.log("file: ", value[i]);
-     formData.append(`${newKey}[]`, value[i]);
-    }
-   } else if (Array.isArray(value)) {
-    console.log("Array: ", value);
+      if (value instanceof FileList) {
+        for (let i = 0; i < value.length; i++) {
+          console.log("file: ", value[i]);
+          formData.append(`${newKey}[]`, value[i]);
+        }
+      } else if (Array.isArray(value)) {
+        console.log("Array: ", value);
 
-    value.forEach((item, index) => {
-     if (typeof item === "object" && item !== null) {
-      convertDataToFormData(item, formData, `${newKey}[${index}]`);
-     } else {
-      formData.append(`${newKey}[]`, item);
-     }
-    });
-   } else if (value instanceof File) {
-    formData.append(`${newKey}[]`, value);
-   } else if (value instanceof Date) {
-    formData.append(newKey, value.toISOString()); // Convert Date to ISO string before appending
-   } else if (
-    typeof value === "object" &&
-    value !== null &&
-    !(value instanceof FileList)
-   ) {
-    convertDataToFormData(value, formData, newKey);
-   } else if (value instanceof FileList) {
-    console.log("fileList: ", value);
-    for (let i = 0; i < value.length; i++) {
-     console.log("file: ", value[i]);
-     formData.append(`${newKey}[]`, value[i]);
+        value.forEach((item, index) => {
+          if (typeof item === "object" && item !== null) {
+            convertDataToFormData(item, formData, `${newKey}[${index}]`);
+          } else {
+            formData.append(`${newKey}[]`, item);
+          }
+        });
+      } else if (value instanceof File) {
+        formData.append(`${newKey}[]`, value);
+      } else if (value instanceof Date) {
+        formData.append(newKey, value.toISOString()); // Convert Date to ISO string before appending
+      } else if (
+        typeof value === "object" &&
+        value !== null &&
+        !(value instanceof FileList)
+      ) {
+        convertDataToFormData(value, formData, newKey);
+      } else if (value instanceof FileList) {
+        console.log("fileList: ", value);
+        for (let i = 0; i < value.length; i++) {
+          console.log("file: ", value[i]);
+          formData.append(`${newKey}[]`, value[i]);
+        }
+      } else {
+        formData.append(newKey, value);
+      }
     }
-   } else {
-    formData.append(newKey, value);
-   }
   }
- }
- return formData;
+  return formData;
 }
