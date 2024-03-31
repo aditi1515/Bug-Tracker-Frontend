92b0f8cc7ef94af5861754c3145f73e14e7669f9
Aditi Gemini <aditi152003@gmail.com>
2024-03-31 15:29:36 +0530

    ticket dashboard

--- Company/tabs/projects/project/dashboard/people_dashboard/people.dashboard.controller.js
+++ Company/tabs/projects/project/dashboard/people_dashboard/people.dashboard.controller.js
@@ -1,46 +1,46 @@
 function PeopleDashboardInProjectController(
- $scope,
- $state,
- AnalyticsService,
- ProjectService
+  $scope,
+  $state,
+  AnalyticsService,
+  ProjectService
 ) {
- $scope.projectId = $state.params.projectId;
- $scope.projectDetails = $scope;
- $scope.dateData = {};
+  $scope.projectId = $state.params.projectId;
+  $scope.projectDetails = {};
+  $scope.dateData = {};
 
- console.log("projectDetails: ", $scope.projectDetails);
- function getUsersInProject() {
-  AnalyticsService.getUsersInProject($scope.projectId).then(function (
-   response
-  ) {
-   $scope.usersInProject = response.data;
-  });
- }
+  console.log("projectDetails: ", $scope.projectDetails);
+  function getUsersInProject() {
+    AnalyticsService.getUsersInProject($scope.projectId).then(function (
+      response
+    ) {
+      $scope.usersInProject = response.data;
+    });
+  }
 
- getUsersInProject();
+  getUsersInProject();
 
- function fetchProjectById() {
-  ProjectService.getProjectById($scope.projectId).then(function (response) {
-   $scope.projectDetails = response.data.project;
-   $scope.dateData = {
-    timePassed: Math.ceil(
-     (new Date() - new Date($scope.projectDetails.createdAt)) /
-      (1000 * 60 * 60 * 24)
-    ),
-    timeLeft: Math.ceil(
-     (new Date($scope.projectDetails.dueDate) - new Date()) /
-      (1000 * 60 * 60 * 24)
-    ) ,
-   };
-  });
- }
- fetchProjectById();
+  function fetchProjectById() {
+    ProjectService.getProjectById($scope.projectId).then(function (response) {
+      $scope.projectDetails = response.data.project;
+      $scope.dateData = {
+        timePassed: Math.ceil(
+          (new Date() - new Date($scope.projectDetails.createdAt)) /
+            (1000 * 60 * 60 * 24)
+        ),
+        timeLeft: Math.ceil(
+          (new Date($scope.projectDetails.dueDate) - new Date()) /
+            (1000 * 60 * 60 * 24)
+        ),
+      };
+    });
+  }
+  fetchProjectById();
 }
 
 trackflow.controller("PeopleDashboardInProjectController", [
- "$scope",
- "$state",
- "AnalyticsService",
- "ProjectService",
- PeopleDashboardInProjectController,
+  "$scope",
+  "$state",
+  "AnalyticsService",
+  "ProjectService",
+  PeopleDashboardInProjectController,
 ]);
